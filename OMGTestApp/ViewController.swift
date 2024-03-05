7a79dd81a5b0bb910ec311bafdaffe49c7755a71
Magzhan Zhumaly <magzhanzhumaly@gmail.com>
2024-03-05 10:59:53 +0500

    Initial Commit

---
+++ OMGTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OMGTestApp
+//
+//  Created by Magzhan Zhumaly on 05.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
